43c1f71275203ad0298993af6eb19d75af4f3a8c
Eri Yawata <yawata.eri@gmail.com>
2024-10-23 21:44:55 +0900

    initial commit

--- polls/views.py
+++ polls/views.py
@@ -1,4 +1,21 @@
 from django.shortcuts import HttpResponse
 
+# Question “index” page – displays the latest few questions.
+# Question “detail” page – displays a question text, with no results but with a form to vote.
+# Question “results” page – displays results for a particular question.
+
+
 def index(request):
     return HttpResponse("Hello World. You're at the polls index")
+
+
+def detail(request, question_id):
+    return HttpResponse("You're looking at question %s" % question_id)
+
+
+def results(request, question_id):
+    return HttpResponse("You're looking at the resuls for question %s" % question_id)
+
+
+def vote(request, question_id):
+    return HttpResponse("You're voting for question %s" % question_id)
